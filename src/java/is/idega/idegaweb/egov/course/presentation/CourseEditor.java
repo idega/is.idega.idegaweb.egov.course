87192c7f52b14a2b10064b9ea7df3c5d857bb28d
Thorhallur Helgason <laddi@idega.com>
2009-05-18 10:55:51 +0000

    Added possibility to manually select courses to show in application.

--- src/java/is/idega/idegaweb/egov/course/presentation/CourseEditor.java
+++ src/java/is/idega/idegaweb/egov/course/presentation/CourseEditor.java
@@ -548,6 +548,7 @@ public class CourseEditor extends CourseBlock {
 		if (showLegend) {
 			Lists list = new Lists();
 			list.setStyleClass("legend");
+			form.add(list);
 
 			ListItem item = new ListItem();
 			item.setStyleClass("openForRegistration");
