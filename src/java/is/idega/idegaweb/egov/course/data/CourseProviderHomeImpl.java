5893367587e78351401220d110bd2caad2c7115c
lapiukshtiss <martynas.stake@gmail.com>
2013-12-20 17:33:00 +0200

    Merge fixes

--- src/java/is/idega/idegaweb/egov/course/data/CourseProviderHomeImpl.java
+++ src/java/is/idega/idegaweb/egov/course/data/CourseProviderHomeImpl.java
@@ -3,82 +3,82 @@
  *
  * Idega Software hf. Source Code Licence Agreement x
  *
- * This agreement, made this 10th of February 2006 by and between 
- * Idega Software hf., a business formed and operating under laws 
- * of Iceland, having its principal place of business in Reykjavik, 
- * Iceland, hereinafter after referred to as "Manufacturer" and Agura 
+ * This agreement, made this 10th of February 2006 by and between
+ * Idega Software hf., a business formed and operating under laws
+ * of Iceland, having its principal place of business in Reykjavik,
+ * Iceland, hereinafter after referred to as "Manufacturer" and Agura
  * IT hereinafter referred to as "Licensee".
- * 1.  License Grant: Upon completion of this agreement, the source 
- *     code that may be made available according to the documentation for 
- *     a particular software product (Software) from Manufacturer 
- *     (Source Code) shall be provided to Licensee, provided that 
- *     (1) funds have been received for payment of the License for Software and 
- *     (2) the appropriate License has been purchased as stated in the 
- *     documentation for Software. As used in this License Agreement, 
- *     Licensee shall also mean the individual using or installing 
- *     the source code together with any individual or entity, including 
- *     but not limited to your employer, on whose behalf you are acting 
- *     in using or installing the Source Code. By completing this agreement, 
- *     Licensee agrees to be bound by the terms and conditions of this Source 
- *     Code License Agreement. This Source Code License Agreement shall 
- *     be an extension of the Software License Agreement for the associated 
- *     product. No additional amendment or modification shall be made 
- *     to this Agreement except in writing signed by Licensee and 
+ * 1.  License Grant: Upon completion of this agreement, the source
+ *     code that may be made available according to the documentation for
+ *     a particular software product (Software) from Manufacturer
+ *     (Source Code) shall be provided to Licensee, provided that
+ *     (1) funds have been received for payment of the License for Software and
+ *     (2) the appropriate License has been purchased as stated in the
+ *     documentation for Software. As used in this License Agreement,
+ *     Licensee shall also mean the individual using or installing
+ *     the source code together with any individual or entity, including
+ *     but not limited to your employer, on whose behalf you are acting
+ *     in using or installing the Source Code. By completing this agreement,
+ *     Licensee agrees to be bound by the terms and conditions of this Source
+ *     Code License Agreement. This Source Code License Agreement shall
+ *     be an extension of the Software License Agreement for the associated
+ *     product. No additional amendment or modification shall be made
+ *     to this Agreement except in writing signed by Licensee and
  *     Manufacturer. This Agreement is effective indefinitely and once
- *     completed, cannot be terminated. Manufacturer hereby grants to 
- *     Licensee a non-transferable, worldwide license during the term of 
- *     this Agreement to use the Source Code for the associated product 
- *     purchased. In the event the Software License Agreement to the 
- *     associated product is terminated; (1) Licensee's rights to use 
- *     the Source Code are revoked and (2) Licensee shall destroy all 
- *     copies of the Source Code including any Source Code used in 
+ *     completed, cannot be terminated. Manufacturer hereby grants to
+ *     Licensee a non-transferable, worldwide license during the term of
+ *     this Agreement to use the Source Code for the associated product
+ *     purchased. In the event the Software License Agreement to the
+ *     associated product is terminated; (1) Licensee's rights to use
+ *     the Source Code are revoked and (2) Licensee shall destroy all
+ *     copies of the Source Code including any Source Code used in
  *     Licensee's applications.
  * 2.  License Limitations
- *     2.1 Licensee may not resell, rent, lease or distribute the 
- *         Source Code alone, it shall only be distributed as a 
+ *     2.1 Licensee may not resell, rent, lease or distribute the
+ *         Source Code alone, it shall only be distributed as a
  *         compiled component of an application.
- *     2.2 Licensee shall protect and keep secure all Source Code 
- *         provided by this this Source Code License Agreement. 
- *         All Source Code provided by this Agreement that is used 
+ *     2.2 Licensee shall protect and keep secure all Source Code
+ *         provided by this this Source Code License Agreement.
+ *         All Source Code provided by this Agreement that is used
  *         with an application that is distributed or accessible outside
- *         Licensee's organization (including use from the Internet), 
- *         must be protected to the extent that it cannot be easily 
+ *         Licensee's organization (including use from the Internet),
+ *         must be protected to the extent that it cannot be easily
  *         extracted or decompiled.
- *     2.3 The Licensee shall not resell, rent, lease or distribute 
- *         the products created from the Source Code in any way that 
+ *     2.3 The Licensee shall not resell, rent, lease or distribute
+ *         the products created from the Source Code in any way that
  *         would compete with Idega Software.
- *     2.4 Manufacturer's copyright notices may not be removed from 
+ *     2.4 Manufacturer's copyright notices may not be removed from
  *         the Source Code.
- *     2.5 All modifications on the source code by Licencee must 
+ *     2.5 All modifications on the source code by Licencee must
  *         be submitted to or provided to Manufacturer.
- * 3.  Copyright: Manufacturer's source code is copyrighted and contains 
- *     proprietary information. Licensee shall not distribute or 
- *     reveal the Source Code to anyone other than the software 
- *     developers of Licensee's organization. Licensee may be held 
- *     legally responsible for any infringement of intellectual property 
- *     rights that is caused or encouraged by Licensee's failure to abide 
- *     by the terms of this Agreement. Licensee may make copies of the 
- *     Source Code provided the copyright and trademark notices are 
- *     reproduced in their entirety on the copy. Manufacturer reserves 
+ * 3.  Copyright: Manufacturer's source code is copyrighted and contains
+ *     proprietary information. Licensee shall not distribute or
+ *     reveal the Source Code to anyone other than the software
+ *     developers of Licensee's organization. Licensee may be held
+ *     legally responsible for any infringement of intellectual property
+ *     rights that is caused or encouraged by Licensee's failure to abide
+ *     by the terms of this Agreement. Licensee may make copies of the
+ *     Source Code provided the copyright and trademark notices are
+ *     reproduced in their entirety on the copy. Manufacturer reserves
  *     all rights not specifically granted to Licensee.
  *
- * 4.  Warranty & Risks: Although efforts have been made to assure that the 
- *     Source Code is correct, reliable, date compliant, and technically 
- *     accurate, the Source Code is licensed to Licensee as is and without 
- *     warranties as to performance of merchantability, fitness for a 
- *     particular purpose or use, or any other warranties whether 
- *     expressed or implied. Licensee's organization and all users 
- *     of the source code assume all risks when using it. The manufacturers, 
- *     distributors and resellers of the Source Code shall not be liable 
- *     for any consequential, incidental, punitive or special damages 
- *     arising out of the use of or inability to use the source code or 
- *     the provision of or failure to provide support services, even if we 
- *     have been advised of the possibility of such damages. In any case, 
- *     the entire liability under any provision of this agreement shall be 
- *     limited to the greater of the amount actually paid by Licensee for the 
- *     Software or 5.00 USD. No returns will be provided for the associated 
- *     License that was purchased to become eligible to receive the Source 
- *     Code after Licensee receives the source code. 
+ * 4.  Warranty & Risks: Although efforts have been made to assure that the
+ *     Source Code is correct, reliable, date compliant, and technically
+ *     accurate, the Source Code is licensed to Licensee as is and without
+ *     warranties as to performance of merchantability, fitness for a
+ *     particular purpose or use, or any other warranties whether
+ *     expressed or implied. Licensee's organization and all users
+ *     of the source code assume all risks when using it. The manufacturers,
+ *     distributors and resellers of the Source Code shall not be liable
+ *     for any consequential, incidental, punitive or special damages
+ *     arising out of the use of or inability to use the source code or
+ *     the provision of or failure to provide support services, even if we
+ *     have been advised of the possibility of such damages. In any case,
+ *     the entire liability under any provision of this agreement shall be
+ *     limited to the greater of the amount actually paid by Licensee for the
+ *     Software or 5.00 USD. No returns will be provided for the associated
+ *     License that was purchased to become eligible to receive the Source
+ *     Code after Licensee receives the source code.
  */
 package is.idega.idegaweb.egov.course.data;
 
@@ -108,14 +108,14 @@ import com.idega.util.expression.ELUtil;
 
 /**
  * <p>Implementation for {@link CourseProviderHome}</p>
- * <p>You can report about problems to: 
+ * <p>You can report about problems to:
  * <a href="mailto:martynas@idega.is">Martynas Stakė</a></p>
  *
  * @version 1.0.0 Oct 23, 2013
  * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
  * @param <T>
  */
-public class CourseProviderHomeImpl extends IDOFactory 
+public class CourseProviderHomeImpl extends IDOFactory
 		implements CourseProviderHome {
 
 	private static final long serialVersionUID = -2095902709213742089L;
@@ -133,7 +133,7 @@ public class CourseProviderHomeImpl extends IDOFactory
 	 * @see is.idega.idegaweb.egov.course.data.CourseProviderHome#findAll()
 	 */
 	@Override
-	public Collection<? extends CourseProvider> find() {
+	public <P extends CourseProvider> Collection<P> find() {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		if (entity == null) {
 			return java.util.Collections.emptyList();
@@ -144,7 +144,7 @@ public class CourseProviderHomeImpl extends IDOFactory
 			return this.getEntityCollectionForPrimaryKeys(ids);
 		} catch (FinderException e) {
 			java.util.logging.Logger.getLogger(getClass().getName()).log(
-					Level.WARNING, "Failed to get primary keys of " + 
+					Level.WARNING, "Failed to get primary keys of " +
 					getEntityInterfaceClass().getClass().getSimpleName());
 		}
 
@@ -160,20 +160,20 @@ public class CourseProviderHomeImpl extends IDOFactory
 		if (StringUtil.isEmpty(primaryKey)) {
 			return null;
 		}
-		
+
 		try {
 			CourseProvider provider = (CourseProvider) findSubTypeByPrimaryKeyIDO(primaryKey);
 			return provider;
 		} catch (FinderException e) {
 			java.util.logging.Logger.getLogger(getClass().getName()).log(
-					Level.WARNING, 
+					Level.WARNING,
 					"Failed to get " + getEntityInterfaceClass().getClass().getSimpleName() + 
 					" by primary key: " + primaryKey);
 		}
 
 		return null;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * @see is.idega.idegaweb.egov.course.data.CourseProviderHome#find(java.util.Collection)
@@ -181,8 +181,8 @@ public class CourseProviderHomeImpl extends IDOFactory
 	@Override
 	public Collection<? extends CourseProvider> find(Collection<String> primaryKeys) {
 		if (ListUtil.isEmpty(primaryKeys)) {
-			return Collections.emptyList();
-		}
+		return Collections.emptyList();
+	}
 
 		ArrayList<CourseProvider> providers = new ArrayList<CourseProvider>(primaryKeys.size());
 		for (String key : primaryKeys) {
@@ -220,7 +220,7 @@ public class CourseProviderHomeImpl extends IDOFactory
 	 * @see is.idega.idegaweb.egov.course.data.CourseProviderHome#findByType(java.util.Collection)
 	 */
 	@Override
-	public Collection<? extends CourseProvider> findByType(Collection<? extends CourseProviderType> types) {
+	public <P extends CourseProvider> Collection<P> findByType(Collection<? extends CourseProviderType> types) {
 		if (ListUtil.isEmpty(types)) {
 			return Collections.emptyList();
 		}
@@ -229,13 +229,13 @@ public class CourseProviderHomeImpl extends IDOFactory
 		if (entity == null) {
 			return java.util.Collections.emptyList();
 		}
-		
+
 		Collection<Object> primaryKeys = entity.ejbFindByType(types);
 		try {
 			return getEntityCollectionForPrimaryKeys(primaryKeys);
 		} catch (FinderException e) {
-			Logger.getLogger(getClass().getName()).log(
-					Level.WARNING, 
+			java.util.logging.Logger.getLogger(getClass().getName()).log(
+					Level.WARNING,
 					"Failed to get " + getEntityInterfaceClass().getSimpleName() + 
 					"'s by id's: '" + primaryKeys + "' cause of: ", e);
 		}
@@ -258,13 +258,13 @@ public class CourseProviderHomeImpl extends IDOFactory
 		if (entity == null) {
 			return java.util.Collections.emptyList();
 		}
-		
+
 		Collection<Object> primaryKeys = entity.ejbFindByArea(areas);
 		try {
 			return getEntityCollectionForPrimaryKeys(primaryKeys);
 		} catch (FinderException e) {
 			java.util.logging.Logger.getLogger(getClass().getName()).log(
-					Level.WARNING, 
+					Level.WARNING,
 					"Failed to get " + getEntityInterfaceClass().getSimpleName() + 
 					"'s by id's: '" + primaryKeys + "' cause of: ", e);
 		}
@@ -277,7 +277,7 @@ public class CourseProviderHomeImpl extends IDOFactory
 	 * @see is.idega.idegaweb.egov.course.data.CourseProviderHome#find(java.util.Collection, java.util.Collection)
 	 */
 	@Override
-	public Collection<? extends CourseProvider> find(
+	public <P extends CourseProvider> Collection<P> find(
 			Collection<? extends CourseProviderArea> areas,
 			Collection<? extends CourseProviderType> types) {
 		if (ListUtil.isEmpty(areas) || ListUtil.isEmpty(types)) {
@@ -288,20 +288,20 @@ public class CourseProviderHomeImpl extends IDOFactory
 		if (entity == null) {
 			return java.util.Collections.emptyList();
 		}
-		
+
 		Collection<Object> primaryKeys = entity.ejbFind(types, areas);
 		try {
 			return getEntityCollectionForPrimaryKeys(primaryKeys);
 		} catch (FinderException e) {
 			java.util.logging.Logger.getLogger(getClass().getName()).log(
-					Level.WARNING, 
-					"Failed to get " + getEntityInterfaceClass().getName() + 
+					Level.WARNING,
+					"Failed to get " + getEntityInterfaceClass().getName() +
 					"'s by id's: '" + primaryKeys + "' cause of: ", e);
 		}
 
 		return Collections.emptyList();
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * @see is.idega.idegaweb.egov.course.data.CourseProviderHome#update(is.idega.idegaweb.egov.course.data.CourseProvider, com.idega.user.data.Group, com.idega.core.location.data.PostalCode, is.idega.idegaweb.egov.course.data.CourseProviderArea)
@@ -311,7 +311,7 @@ public class CourseProviderHomeImpl extends IDOFactory
 			CourseProvider provider,
 			Group group,
 			PostalCode postalCode,
-			CourseProviderArea providerArea, 
+			CourseProviderArea providerArea,
 			boolean notify) {
 
 		String primaryKey = null;
@@ -326,7 +326,7 @@ public class CourseProviderHomeImpl extends IDOFactory
 		if (group != null) {
 			name = group.getName();
 			info = group.getDescription();
-			
+
 			Collection<Phone> phones = group.getPhones();
 			if (!ListUtil.isEmpty(phones)) {
 				phone = phones.iterator().next().getNumber();
@@ -337,8 +337,8 @@ public class CourseProviderHomeImpl extends IDOFactory
 				addresses = group.getAddresses(null);
 			} catch (Exception e) {
 				java.util.logging.Logger.getLogger(getClass().getName()).log(
-						Level.WARNING, 
-						"Failed to find adresses for group: '" + name + 
+						Level.WARNING,
+						"Failed to find adresses for group: '" + name +
 						"' cause of: ", e);
 			}
 
@@ -362,11 +362,11 @@ public class CourseProviderHomeImpl extends IDOFactory
 		String webPage = null;
 		String courseProviderAreaId = null;
 
-		return update(primaryKey, name, providerId, communeId, phone, 
-				webPage, info, organizationNumber , zipArea, zipCode, 
+		return update(primaryKey, name, providerId, communeId, phone,
+				webPage, info, organizationNumber , zipArea, zipCode,
 				address, courseProviderAreaId, null, null, notify);
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * @see is.idega.idegaweb.egov.course.data.CourseProviderHome#update(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.Boolean, java.lang.Boolean)
@@ -377,7 +377,7 @@ public class CourseProviderHomeImpl extends IDOFactory
 			String info, String organizationNumber, String zipArea,
 			String zipCode, String address, String courseProviderAreaId,
 			Boolean hasPreCare, Boolean hasPostCare, boolean notify) {
-		
+
 		CourseProvider courseProvider = null;
 		if (!StringUtil.isEmpty(primaryKey)) {
 			courseProvider = find(primaryKey);
@@ -388,8 +388,8 @@ public class CourseProviderHomeImpl extends IDOFactory
 				courseProvider = super.createIDO();
 			} catch (CreateException e) {
 				java.util.logging.Logger.getLogger(getClass().getName()).log(
-						Level.WARNING, 
-						"Failed to create " + CourseProvider.class.getName() + 
+						Level.WARNING,
+						"Failed to create " + CourseProvider.class.getName() +
 						" cause of: ", e);
 				return null;
 			}
@@ -450,13 +450,13 @@ public class CourseProviderHomeImpl extends IDOFactory
 		try {
 			courseProvider.store();
 			java.util.logging.Logger.getLogger(getClass().getName()).info(
-					CourseProvider.class.getName() + 
-					" by id: " + courseProvider.getPrimaryKey().toString() + 
+					CourseProvider.class.getName() +
+					" by id: " + courseProvider.getPrimaryKey().toString() +
 					" stored!");
 		} catch (Exception e) {
 			java.util.logging.Logger.getLogger(getClass().getName()).log(
-					Level.WARNING, 
-					"Failed to create " + CourseProvider.class.getName() + 
+					Level.WARNING,
+					"Failed to create " + CourseProvider.class.getName() +
 					" cause of: ", e);
 			return null;
 		}
@@ -495,7 +495,7 @@ public class CourseProviderHomeImpl extends IDOFactory
 	 */
 	@Override
 	public void remove(String primaryKey) {
-		remove(find(primaryKey));		
+		remove(find(primaryKey));
 	}
 
 	private CourseProviderUserHome courseProviderUserHome = null;
@@ -506,8 +506,8 @@ public class CourseProviderHomeImpl extends IDOFactory
 				this.courseProviderUserHome = (CourseProviderUserHome) IDOLookup.getHome(CourseProviderUser.class);
 			} catch (IDOLookupException e) {
 				java.util.logging.Logger.getLogger(getClass().getName()).log(
-						Level.WARNING, 
-						"Failed to get " + CourseProviderUserHome.class.getName() + 
+						Level.WARNING,
+						"Failed to get " + CourseProviderUserHome.class.getName() +
 						" cause of: ", e);
 			}
 		}
