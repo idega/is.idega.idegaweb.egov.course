1580918231f99c81b4d3b6db541fe21cc96c683d
lapiukshtiss <martynas.stake@gmail.com>
2014-02-10 15:04:40 +0200

    Autoformatting, employees, birthdates

--- src/java/is/idega/idegaweb/egov/course/business/CourseWriter.java
+++ src/java/is/idega/idegaweb/egov/course/business/CourseWriter.java
@@ -36,8 +36,10 @@ import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 
 import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
 import com.idega.core.file.util.MimeTypeUtil;
 import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWResourceBundle;
@@ -48,15 +50,17 @@ import com.idega.io.MemoryFileBuffer;
 import com.idega.io.MemoryInputStream;
 import com.idega.io.MemoryOutputStream;
 import com.idega.presentation.IWContext;
+import com.idega.util.CoreConstants;
 import com.idega.util.CoreUtil;
 import com.idega.util.IWTimestamp;
+import com.idega.util.ListUtil;
 import com.idega.util.StringHandler;
 import com.idega.util.StringUtil;
 
 public class CourseWriter extends DownloadWriter implements MediaWritable {
 
 	private MemoryFileBuffer buffer = null;
-	private CourseBusiness business;
+	private CourseBusiness business = null;
 	private Locale locale;
 	private IWResourceBundle iwrb;
 
@@ -65,8 +69,22 @@ public class CourseWriter extends DownloadWriter implements MediaWritable {
 	public final static String PARAMETER_PROVIDER_ID = "provider_id";
 	public final static String PARAMETER_GROUP_ID = "group_id";
 	public final static String PARAMETER_SHOW_NOT_YET_ACTIVE = "show_not_yet_active";
+	
+	public CourseWriter() {}
 
-	public CourseWriter() {
+	protected CourseBusiness getCourseBusiness() {
+		if (this.business == null) {
+			try {
+				this.business = IBOLookup.getServiceInstance(
+						CoreUtil.getIWContext(), CourseBusiness.class);
+			} catch (IBOLookupException e) {
+				Logger.getLogger(getClass().getName()).log(Level.WARNING, 
+						"Failed to get " + CourseBusiness.class.getSimpleName() + 
+						" cause of: ", e);
+			}
+		}
+
+		return this.business;
 	}
 
 	/**
@@ -115,7 +133,7 @@ public class CourseWriter extends DownloadWriter implements MediaWritable {
 				Date toDate = getParsedDate(
 						iwc.getParameter(CourseBlock.PARAMETER_TO_DATE));
 
-				Collection<Course> courses = business.getCourses(
+				Collection<Course> courses = getCourseBusiness().getCourses(
 						-1, 
 						getCourseSession(iwc).getProvider().getPrimaryKey().toString(), 
 						iwc.getParameter(CourseBlock.PARAMETER_SCHOOL_TYPE_PK), 
@@ -149,104 +167,132 @@ public class CourseWriter extends DownloadWriter implements MediaWritable {
 				baos.write(mis.read());
 			}
 			baos.writeTo(out);
+			mis.close();
 		}
 		else {
 			System.err.println("buffer is null");
 		}
 	}
 
-	public MemoryFileBuffer writeXLS(IWContext iwc, Collection courses) throws Exception {
+	public MemoryFileBuffer writeXLS(IWContext iwc, Collection<Course> courses) throws Exception {
 		MemoryFileBuffer buffer = new MemoryFileBuffer();
 		MemoryOutputStream mos = new MemoryOutputStream(buffer);
-		if (!courses.isEmpty()) {
-			HSSFWorkbook wb = new HSSFWorkbook();
-			HSSFSheet sheet = wb.createSheet(StringHandler.shortenToLength(this.schoolTypeName, 30));
-			sheet.setColumnWidth((short) 0, (short) (24 * 256));
-			sheet.setColumnWidth((short) 1, (short) (24 * 256));
-			sheet.setColumnWidth((short) 2, (short) (8 * 256));
-			sheet.setColumnWidth((short) 3, (short) (8 * 256));
-			sheet.setColumnWidth((short) 4, (short) (12 * 256));
-			sheet.setColumnWidth((short) 5, (short) (12 * 256));
-			sheet.setColumnWidth((short) 6, (short) (8 * 256));
-			sheet.setColumnWidth((short) 7, (short) (8 * 256));
-			sheet.setColumnWidth((short) 8, (short) (20 * 256));
-			HSSFFont font = wb.createFont();
-			font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-			font.setFontHeightInPoints((short) 12);
-			HSSFCellStyle style = wb.createCellStyle();
-			style.setFont(font);
+		
+		HSSFWorkbook wb = new HSSFWorkbook();
+		HSSFSheet sheet = wb.createSheet(StringHandler.shortenToLength(this.schoolTypeName, 30));
 
-			HSSFFont bigFont = wb.createFont();
-			bigFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-			bigFont.setFontHeightInPoints((short) 13);
-			HSSFCellStyle bigStyle = wb.createCellStyle();
-			bigStyle.setFont(bigFont);
+		/* Font style */
+		HSSFFont font = wb.createFont();
+		font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+		font.setFontHeightInPoints((short) 12);
+		HSSFCellStyle style = wb.createCellStyle();
+		style.setFont(font);
 
-			int cellRow = 0;
-			HSSFRow row = sheet.createRow(cellRow++);
-			HSSFCell cell = row.createCell((short) 0);
-			cell.setCellValue(this.schoolTypeName);
-			cell.setCellStyle(bigStyle);
-			cell = row.createCell((short) 1);
+		/* Header style */
+		HSSFFont bigFont = wb.createFont();
+		bigFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+		bigFont.setFontHeightInPoints((short) 13);
+		HSSFCellStyle bigStyle = wb.createCellStyle();
+		bigStyle.setFont(bigFont);
 
-			row = sheet.createRow(cellRow++);
+		/* Setting header */
+		int cellRow = 0;
+		HSSFRow row = sheet.createRow(cellRow++);
+		HSSFCell cell = row.createCell(0);
+		cell.setCellValue(this.schoolTypeName);
+		cell.setCellStyle(bigStyle);
+		cell = row.createCell(1);
 
-			short iCell = 0;
-			row = sheet.createRow(cellRow++);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("type", "Type"));
-			cell.setCellStyle(style);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("course", "Course"));
-			cell.setCellStyle(style);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("from", "From"));
-			cell.setCellStyle(style);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("to", "To"));
-			cell.setCellStyle(style);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("date_from", "Date from"));
-			cell.setCellStyle(style);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("date_to", "Date to"));
-			cell.setCellStyle(style);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("max", "Max"));
-			cell.setCellStyle(style);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("free_places", "Free places"));
-			cell.setCellStyle(style);
-			cell = row.createCell(iCell++);
-			cell.setCellValue(this.iwrb.getLocalizedString("employee", "Employee"));
-			cell.setCellStyle(style);
+		row = sheet.createRow(cellRow++);
 
-			Iterator iter = courses.iterator();
-			while (iter.hasNext()) {
+		int iCell = 0;
+		row = sheet.createRow(cellRow++);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("type", "Type"));
+		cell.setCellStyle(style);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("course", "Course"));
+		cell.setCellStyle(style);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("from", "From"));
+		cell.setCellStyle(style);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("to", "To"));
+		cell.setCellStyle(style);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("date_from", "Date from"));
+		cell.setCellStyle(style);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("date_to", "Date to"));
+		cell.setCellStyle(style);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("max", "Max"));
+		cell.setCellStyle(style);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("free_places", "Free places"));
+		cell.setCellStyle(style);
+		cell = row.createCell(iCell++);
+		cell.setCellValue(this.iwrb.getLocalizedString("employee", "Employee"));
+		cell.setCellStyle(style);
+
+		/* Setting data if exist */
+		if (!ListUtil.isEmpty(courses)) {
+			for (Course course : courses) {
 				row = sheet.createRow(cellRow++);
 				iCell = 0;
 
-				Course course = (Course) iter.next();
 				CourseType type = course.getCourseType();
-				CoursePrice price = course.getPrice();
 				IWTimestamp dateFrom = new IWTimestamp(course.getStartDate());
 				IWTimestamp dateTo = new IWTimestamp(course.getStartDate());
+				
+				CoursePrice price = course.getPrice();
 				if (price != null) {
 					dateTo.addDays(price.getNumberOfDays());
 				}
 
 				row.createCell(iCell++).setCellValue(type.getName());
 				row.createCell(iCell++).setCellValue(course.getName());
-				row.createCell(iCell++).setCellValue(String.valueOf(course.getBirthyearFrom()));
-				row.createCell(iCell++).setCellValue(String.valueOf(course.getBirthyearTo()));
+
+				/* Birthdays */
+				HSSFCell bFromCell = row.createCell(iCell++);
+				int birthyearFrom = course.getBirthyearFrom();
+				if (birthyearFrom > 0) {
+					bFromCell.setCellValue(String.valueOf(birthyearFrom));
+				} else {
+					bFromCell.setCellValue(CoreConstants.MINUS);
+				}
+
+				HSSFCell bToCell = row.createCell(iCell++);
+				int birthyearTo = course.getBirthyearTo();
+				if (birthyearTo > 0) {
+					bToCell.setCellValue(String.valueOf(birthyearTo));
+				} else {
+					bToCell.setCellValue(CoreConstants.MINUS);
+				}
+
 				row.createCell(iCell++).setCellValue(dateFrom.getLocaleDate(iwc.getCurrentLocale(), IWTimestamp.SHORT));
 				row.createCell(iCell++).setCellValue(dateTo.getLocaleDate(iwc.getCurrentLocale(), IWTimestamp.SHORT));
 				row.createCell(iCell++).setCellValue(String.valueOf(course.getMax()));
 				row.createCell(iCell++).setCellValue(String.valueOf(business.getNumberOfFreePlaces(course)));
-				row.createCell(iCell++).setCellValue("-");
+
+				/* Employee */
+				HSSFCell employeeCell = row.createCell(iCell++);
+				String courseEmployee = course.getUser();
+				if (!StringUtil.isEmpty(courseEmployee)) {
+					employeeCell.setCellValue(courseEmployee);
+				} else {
+					employeeCell.setCellValue(CoreConstants.MINUS);
+				}
 			}
-			wb.write(mos);
 		}
+
+		/* Formatting size */
+		Iterator<Cell> cellIterator = row.cellIterator();
+		while (cellIterator.hasNext()) {
+			sheet.autoSizeColumn(cellIterator.next().getColumnIndex());
+		}
+
+		wb.write(mos);
 		buffer.setMimeType(MimeTypeUtil.MIME_TYPE_EXCEL_2);
 		return buffer;
 	}
