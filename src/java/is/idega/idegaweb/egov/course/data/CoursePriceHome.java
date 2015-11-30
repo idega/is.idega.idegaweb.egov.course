5d1d98f94d7e8e382b97d77742e7918002cc8df9
Valdas Žemaitis <valdas@idega.com>
2015-11-30 16:03:31 +0200

    Using generics

--- src/java/is/idega/idegaweb/egov/course/data/CoursePriceHome.java
+++ src/java/is/idega/idegaweb/egov/course/data/CoursePriceHome.java
@@ -1,13 +1,15 @@
 package is.idega.idegaweb.egov.course.data;
 
 
-import com.idega.data.IDORelationshipException;
-import java.util.Collection;
-import javax.ejb.CreateException;
-import com.idega.data.IDOHome;
-import javax.ejb.FinderException;
 import java.sql.Date;
+import java.util.Collection;
+
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+
 import com.idega.block.school.data.SchoolArea;
+import com.idega.data.IDOHome;
+import com.idega.data.IDORelationshipException;
 
 public interface CoursePriceHome extends IDOHome {
 
@@ -15,11 +17,11 @@ public interface CoursePriceHome extends IDOHome {
 
 	public CoursePrice findByPrimaryKey(Object pk) throws FinderException;
 
-	public Collection findAll() throws FinderException, IDORelationshipException;
+	public Collection<CoursePrice> findAll() throws FinderException, IDORelationshipException;
 
-	public Collection findAll(Date fromDate, Date toDate) throws FinderException, IDORelationshipException;
+	public Collection<CoursePrice> findAll(Date fromDate, Date toDate) throws FinderException, IDORelationshipException;
 
-	public Collection findAll(SchoolArea area, CourseType cType) throws FinderException, IDORelationshipException;
+	public Collection<CoursePrice> findAll(SchoolArea area, CourseType cType) throws FinderException, IDORelationshipException;
 
-	public Collection findAll(SchoolArea area, CourseType cType, Date fromDate, Date toDate) throws FinderException, IDORelationshipException;
+	public Collection<CoursePrice> findAll(SchoolArea area, CourseType cType, Date fromDate, Date toDate) throws FinderException, IDORelationshipException;
 }
