dfa77be1a6d42a78a9a8ebe47c23c6e27a8e41d3
Valdas Žemaitis <valdas@idega.com>
2019-02-12 16:24:03 +0200

    Using generics

--- src/java/is/idega/idegaweb/egov/course/data/CourseCertificateHome.java
+++ src/java/is/idega/idegaweb/egov/course/data/CourseCertificateHome.java
@@ -2,10 +2,12 @@ package is.idega.idegaweb.egov.course.data;
 
 
 import java.util.Collection;
-import javax.ejb.CreateException;
-import com.idega.data.IDOHome;
-import javax.ejb.FinderException;
 import java.util.List;
+
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+
+import com.idega.data.IDOHome;
 import com.idega.user.data.User;
 
 public interface CourseCertificateHome extends IDOHome {
@@ -13,14 +15,14 @@ public interface CourseCertificateHome extends IDOHome {
 	public CourseCertificate create() throws CreateException;
 
 	public CourseCertificate findByPrimaryKey(Object pk) throws FinderException;
-	
+
 	public Collection findAllCertificatesByUser(User user) throws FinderException;
-	
+
 	public Collection findAllCertificatesByUserAndCourse(User user, Course course) throws FinderException;
-	
+
 	public CourseCertificate findByUserAndCourse(User user, Course course) throws FinderException;
-	
-	public Collection findCertificatesByUsersAndValidityAndType(List usersIds, boolean onlyValidCertificates, String certificateTypeId) throws FinderException;
+
+	public Collection<CourseCertificate> findCertificatesByUsersAndValidityAndType(List usersIds, boolean onlyValidCertificates, String certificateTypeId) throws FinderException;
 
 	public CourseCertificate findHighestNumber() throws FinderException;
 }
--- src/java/is/idega/idegaweb/egov/course/data/CourseCertificateHomeImpl.java
+++ src/java/is/idega/idegaweb/egov/course/data/CourseCertificateHomeImpl.java
@@ -2,29 +2,35 @@ package is.idega.idegaweb.egov.course.data;
 
 
 import java.util.Collection;
+import java.util.List;
+
 import javax.ejb.CreateException;
 import javax.ejb.FinderException;
-import java.util.List;
-import com.idega.user.data.User;
+
 import com.idega.data.IDOEntity;
 import com.idega.data.IDOFactory;
+import com.idega.user.data.User;
 
 public class CourseCertificateHomeImpl extends IDOFactory implements CourseCertificateHome {
 
 	private static final long serialVersionUID = -6942609966693835007L;
 
+	@Override
 	public Class getEntityInterfaceClass() {
 		return CourseCertificate.class;
 	}
 
+	@Override
 	public CourseCertificate create() throws CreateException {
 		return (CourseCertificate) super.createIDO();
 	}
 
+	@Override
 	public CourseCertificate findByPrimaryKey(Object pk) throws FinderException {
 		return (CourseCertificate) super.findByPrimaryKeyIDO(pk);
 	}
 
+	@Override
 	public Collection findAllCertificatesByUser(User user) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Collection ids = ((CourseCertificateBMPBean) entity).ejbFindAllByUser(user);
@@ -32,6 +38,7 @@ public class CourseCertificateHomeImpl extends IDOFactory implements CourseCerti
 		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 
+	@Override
 	public Collection findAllCertificatesByUserAndCourse(User user, Course course) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Collection ids = ((CourseCertificateBMPBean) entity).ejbFindAllByUserAndCourse(user, course);
@@ -39,6 +46,7 @@ public class CourseCertificateHomeImpl extends IDOFactory implements CourseCerti
 		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 
+	@Override
 	public CourseCertificate findByUserAndCourse(User user, Course course) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Object pk = ((CourseCertificateBMPBean) entity).ejbFindByUserAndCourse(user, course);
@@ -46,13 +54,15 @@ public class CourseCertificateHomeImpl extends IDOFactory implements CourseCerti
 		return this.findByPrimaryKey(pk);
 	}
 
-	public Collection findCertificatesByUsersAndValidityAndType(List usersIds, boolean onlyValidCertificates, String certificateTypeId) throws FinderException {
+	@Override
+	public Collection<CourseCertificate> findCertificatesByUsersAndValidityAndType(List usersIds, boolean onlyValidCertificates, String certificateTypeId) throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
-		Collection ids = ((CourseCertificateBMPBean) entity).ejbFindByUsersAndValidityAndType(usersIds, onlyValidCertificates, certificateTypeId);
+		Collection<?> ids = ((CourseCertificateBMPBean) entity).ejbFindByUsersAndValidityAndType(usersIds, onlyValidCertificates, certificateTypeId);
 		this.idoCheckInPooledEntity(entity);
 		return this.getEntityCollectionForPrimaryKeys(ids);
 	}
 
+	@Override
 	public CourseCertificate findHighestNumber() throws FinderException {
 		IDOEntity entity = this.idoCheckOutPooledEntity();
 		Object pk = ((CourseCertificateBMPBean) entity).ejbFindHighestNumber();
